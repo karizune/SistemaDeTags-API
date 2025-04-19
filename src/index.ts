59d21b9f269d15de982c18ea82e34647fc98e300
karizune <luanchrystian2@gmail.com>
2025-04-19 08:56:22 -0300

    Adição do cors

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import express from 'express';
 import { Configuration } from './Application/Config/Configuration';
 import { ProductRoutes } from './Route/ProductRoutes';
 import { Dependencies } from './Application/DependencyInjection/Dependencies';
+import cors from 'cors';
 
 const server: express.Express = express();
+server.use(cors()); // Habilita CORS para todas as rotas
 server.use(express.json());
 server.use(new ProductRoutes(Dependencies.ProductController).GetRoutes())
 
